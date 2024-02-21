3ba11ffdd4d695e76abdbb2aad1c3c015c12ce4e
melrodriguesJV <mel1985rod@gmail.com>
2024-02-21 15:56:16 +0100

    move camera

---
+++ Assets/cam_move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_move : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+
+    [SerializeField] private Transform player;
+
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
