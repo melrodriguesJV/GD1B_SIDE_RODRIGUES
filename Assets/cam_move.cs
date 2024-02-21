69f906857a3b4635d45a4d29cfce5d870d8fec7f
melrodriguesJV <mel1985rod@gmail.com>
2024-02-21 16:05:35 +0100

    smooth cam

--- Assets/cam_move.cs
+++ Assets/cam_move.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cam_move : MonoBehaviour
-{
-    [SerializeField] private float speed;
-    private float currentPosX;
-    private Vector3 velocity = Vector3.zero;
-
-
-    [SerializeField] private Transform player;
-
+{   [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 }
