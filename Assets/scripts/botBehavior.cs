9def19b45566b1ce50adbf5ebe25bfedfdabd1f4
melrodriguesJV <mel1985rod@gmail.com>
2024-02-24 21:32:08 +0100

    active ennemy

--- Assets/scripts/botBehavior.cs
+++ Assets/scripts/botBehavior.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class botBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask characterLayer;
+    private float cooldownTimer = Mathf.Infinity;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+
+
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+
+            }
+
+        }
+    }
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.left,0,characterLayer);
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center,boxCollider.bounds.size);
     }
 }
