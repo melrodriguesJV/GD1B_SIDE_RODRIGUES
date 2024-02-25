64723296d71f7799084a25888f92935a8cc2d1ee
melrodriguesJV <mel1985rod@gmail.com>
2024-02-25 18:06:00 +0100

    health install

---
+++ Assets/scripts/health/health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _damage, 0, startingHealth);
+        
+        if(currentHealth > 0)
+        {
+            //player hurt
+            
+        }
+        else
+        {
+            //player dead
+        }
+    }
+}
